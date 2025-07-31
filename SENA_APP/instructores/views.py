1aac88ee663544f4928f14a6303db893a43f67c5
msaydr15 <rr2685346@gmail.com>
2025-07-31 08:02:27 -0500

    registramos el modelo en el admin, se crea la plantilla para mostrar la lista de instructores

--- SENA_APP/instructores/views.py
+++ SENA_APP/instructores/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.template import loader
+from .models import Instructor
 
 # Create your views here.
+def instructores(request):
+    lista_instructores = Instructor.objects.all().order_by('apellido','nombre')
+    template = loader.get_template('lista_instructores.html')
+    context = {
+        'lista_instructores': lista_instructores,
+        'total_instructores': lista_instructores.count(),
+    }
+    return HttpResponse(template.render(context, request))
